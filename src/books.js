71429c25df178fdf8b5ae9bfde2b5c4dd9dd079f
carllix <carloangkisan21@gmail.com>
2025-02-16 09:04:04 +0700

    style: add type of books

--- src/books.js
+++ src/books.js
@@ -1,3 +1,18 @@
+/**
+ * @typedef {Object} Book
+ * @property {string} id 
+ * @property {string} name
+ * @property {number} year
+ * @property {string} author
+ * @property {string} summary
+ * @property {string} publisher
+ * @property {number} pageCount
+ * @property {number} readPage
+ * @property {boolean} reading
+ */
+
+/** @type {Book[]} */
+
 const books = [];
 
 module.exports = books;
